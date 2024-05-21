b2ae827478d41b4c49b5699e805c3bb8f2a717ed
ongheong <87983309+ongheong@users.noreply.github.com>
2024-05-21 11:55:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.26 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    for(c of control){
+        switch(c){
+            case "w": n += 1;
+                break;
+            case "s": n -= 1;
+                break;
+            case "d": n += 10;
+                break;
+            case "a": n -= 10;
+                break;
+        }
+    }
+    return n;
+}

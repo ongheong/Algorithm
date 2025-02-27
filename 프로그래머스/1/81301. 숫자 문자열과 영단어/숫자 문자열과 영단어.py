b8f760f7dcd3884a50d00df63434c9c679eb06ad
ongheong <87983309+ongheong@users.noreply.github.com>
2025-02-27 23:36:34 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,28 +1,8 @@
 def solution(s):
-    nums = dict()
-    nums[0] = 'zero'
-    nums[1] = 'one'
-    nums[2] = 'two'
-    nums[3] = 'three'
-    nums[4] = 'four'
-    nums[5] = 'five'
-    nums[6] = 'six'
-    nums[7] = 'seven'
-    nums[8] = 'eight'
-    nums[9] = 'nine'
-    answer = ''
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
-    while len(s) > 0:
-        if not s[0].isdigit(): # 문자라면
-            for i in range(len(nums)):
-                if nums[i] == s[0: len(nums[i])]:
-                    answer += str(i)
-                    s = s[len(nums[i]):]
-                    break
-        else: # 숫자라면
-            answer += str(s[0])
-            s = s[1:]
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i)) # i는 int이므로 문자열로 바꿔주기(s도 문자니까)
     
-    return int(answer)
-            
-    
+    return int(s)
+        

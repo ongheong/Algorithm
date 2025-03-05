f208d55a587341ffcbd9dd4106a055bb55f9f313
심여은 <jelly503@naver.com>
2025-03-05 20:50:35 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,25 +1,45 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-N = int(input())
-dq = deque()
+n = int(input())
+deq = deque()
 
-for _ in range(N):
-    cmd = input().strip()
-    if "push" in cmd:
-        cmd, num = map(str, cmd.split(" "))
+for _ in range(n):
+    str = input().split()
+    if len(str) > 1:
+        cmd, num = str[0], int(str[1])
+    else:
+        cmd = str[0]
 
-    if cmd == "push_front": dq.appendleft(num)
-    elif cmd == "push_back": dq.append(num)
-    elif cmd == "pop_front":
-        print(-1) if len(dq) == 0 else print(dq.popleft())
-    elif cmd == "pop_back":
-        print(-1) if len(dq) == 0 else print(dq.pop())
-    elif cmd == "size": print(len(dq))
-    elif cmd == "empty":
-        print(1) if len(dq) == 0 else print(0)
-    elif cmd == "front":
-        print(-1) if len(dq) == 0 else print(dq[0])
-    elif cmd == "back":
-        print(-1) if len(dq) == 0 else print(dq[-1])
+    if cmd == 'push_front':
+        deq.appendleft(num)
+    elif cmd == 'push_back':
+        deq.append(num)
+    elif cmd == 'pop_front':
+        if not deq:
+            print(-1)
+            continue
+        print(deq.popleft())
+    elif cmd == 'pop_back':
+        if not deq:
+            print(-1)
+            continue
+        print(deq.pop())
+    elif cmd == 'size':
+        print(len(deq))
+    elif cmd == 'empty':
+        if not deq:
+            print(1)
+        else:
+            print(0)
+    elif cmd == 'front':
+        if not deq:
+            print(-1)
+            continue
+        print(deq[0])
+    elif cmd == 'back':
+        if not deq:
+            print(-1)
+            continue
+        print(deq[-1])

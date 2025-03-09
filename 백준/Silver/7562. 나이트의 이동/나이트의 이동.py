26678badbb6154225c67d4dcff4602e706a1288e
심여은 <jelly503@naver.com>
2025-03-09 23:32:00 +0900

    [Silver I] Title: 나이트의 이동, Time: 1216 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,30 +1,34 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
+
+dx = [1, 1, 2, 2, -1, -1, -2, -2]
+dy = [2, -2, 1, -1, 2, -2, 1, -1]
+
+def bfs(x, y, x_end, y_end):
+    q = deque()
+    q.append([x, y])
+    a[x][y] = 1
+    while q:
+        x, y = q.popleft()
+        if x == x_end and y == y_end:
+            return a[x][y]-1
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < l and 0 <= ny < l:
+                if a[nx][ny] == 0:
+                    q.append([nx, ny])
+                    a[nx][ny] = a[x][y] + 1
 
 tc = int(input())
-d = [[2, 1], [2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]] #우하좌상
-
-
-def bfs(x, y, ansx, ansy):
-    deq = deque()
-    deq.append([x, y])
-    chess[x][y] = 1
-    while deq:
-        x, y = deq.popleft()
-        if x == ansx and y == ansy:
-            print(chess[x][y]-1)
-            return
-        for i in range(8):
-            X, Y = x + d[i][0], y + d[i][1]
-            if 0 <= X < n and 0 <= Y < n and chess[X][Y] == 0:
-                deq.append([X, Y])
-                chess[X][Y] = chess[x][y]+1
-
-for _ in range(tc):
-    n = int(input())
-    chess = [[0]*n for _ in range(n)]
-    x1, y1 = map(int, input().strip().split()) #시작점
-    x2, y2 = map(int, input().strip().split()) #종점
-    bfs(x1, y1, x2, y2)
-
+while tc:
+    l = int(input())
+    a = [[0]*l for _ in range(l)]
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+    if x1 == x2 and y1 == y2:
+        print(0)
+        tc -= 1
+        continue
+    ans = bfs(x1, y1, x2, y2)
+    print(ans)
+    tc -= 1

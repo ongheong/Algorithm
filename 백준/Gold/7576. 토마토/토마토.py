e25323bed6d7af7f1fe967eb6ce8a9c4b0472bd6
심여은 <pudding4271@gmail.com>
2025-03-19 07:10:57 +0900

    [Gold V] Title: 토마토, Time: 1260 ms, Memory: 108628 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,33 +1,39 @@
+import sys
 from collections import deque
-
-m, n = map(int, input().strip().split())
-array = [list(map(int, input().strip().split())) for _ in range(n)]
-queue = deque([])
+input = sys.stdin.readline
+# m은 가로, n은 세로 => box[n][m]
+m, n = map(int, input().split())
+box = [[] for _ in range(n)]
+d = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+answer = 0
 
 for i in range(n):
+    box[i] = list(map(int, input().split()))
+
+queue = deque()
+for i in range(n):
     for j in range(m):
-        if array[i][j] == 1:
+        if box[i][j] == 1:
             queue.append([i, j])
 
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-result = 0
 
 def bfs():
     while queue:
         x, y = queue.popleft()
-        for i in range(4): #4방향 전부 돌면서 탐색
-            nx, ny = dx[i] + x, dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and array[nx][ny] == 0:
+        for i in range(4):
+            nx, ny = x + d[i][0], y + d[i][1]
+            if 0 <= nx < n and 0 <= ny < m and box[nx][ny] == 0:
+                box[nx][ny] = box[x][y] + 1
                 queue.append([nx, ny])
-                array[nx][ny] = array[x][y] + 1
 
-
+# 먼저 bfs를 돌아서 토마토를 익히고, 값을 갱신한다.
 bfs()
 
-for i in array:
-    for j in i:
-        if j == 0:
+for i in range(n):
+    for j in range(m):
+        if box[i][j] == 0: # 익지 않은 토마토가 있으면
             print(-1)
-            exit(0)
-    result = max(result, max(i))
-print(result - 1)
+            exit()
+    answer = max(answer, max(box[i]))
+
+print(answer - 1)

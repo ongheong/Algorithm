fc8fe44931d85fbe4ddc48e5d0e0a1c97ff78cf8
심여은 <pudding4271@gmail.com>
2025-04-21 13:04:49 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,16 @@
+def backtrack(arr, answer, start):
+    if len(answer) == m:
+        print(" ".join(map(str, answer)))
+        return
+    
+    for i in range(start, n):
+        answer.append(arr[i])
+        backtrack(arr, answer, i+1)
+        answer.pop()
+
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort()
+backtrack(arr, [], 0)

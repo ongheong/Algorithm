c79be46b522b4d9cc196bb47ade70f94dc24f7f6
심여은 <pudding4271@gmail.com>
2025-05-06 23:53:02 +0900

    [level 2] Title: 타겟 넘버, Time: 1077.28 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,20 @@
 from collections import deque
 
-def dfs(sum, idx, cnt, numbers, target):
-    if idx == len(numbers):
-        if sum == target:
-            return cnt+1
-        else:
-            return cnt
-    cnt = dfs(sum + numbers[idx], idx + 1, cnt, numbers, target)
-    cnt = dfs(sum - numbers[idx], idx + 1, cnt, numbers, target)
-    return cnt
-
 def solution(numbers, target):
-    answer = dfs(0, 0, 0, numbers, target) #dfs(sum, idx, cnt)
+    answer = 0
+    leng = len(numbers)
+    queue = deque([])
+    queue.append([numbers[0], 0])
+    queue.append([-numbers[0], 0])
+    
+    while queue:
+        value, idx = queue.popleft()
+        idx += 1
+        if idx >= leng:
+            if value == target:
+                answer += 1
+        else:        
+            queue.append([value-numbers[idx], idx])
+            queue.append([value+numbers[idx], idx])
+    
     return answer

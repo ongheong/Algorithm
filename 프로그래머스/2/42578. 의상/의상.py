7714e1d8625237a6a333e6c4dfe2474aa097bb1b
심여은 <pudding4271@gmail.com>
2025-05-08 02:15:50 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,9 +1,10 @@
+from collections import defaultdict
+
 def solution(clothes):
     answer = 1
-    hash = {}
-    for c in clothes:
-        if c[1] in hash: hash[c[1]].append(c[0])
-        else: hash[c[1]] = [c[0]]
-    for key in hash:
-        answer *= len(hash[key])+1 #아무것도 안입는 경우 + 옷가지 수
-    return answer-1 #아무것도 안입는 경우 제외
+    hash = defaultdict(int)
+    for name, type  in clothes:
+        hash[type] += 1
+    for key, value in hash.items():
+        answer *= (value + 1)
+    return answer-1

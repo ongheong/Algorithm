a62a2fb2f930ad9f524bbc4d4c17724d57c1843c
심여은 <pudding4271@gmail.com>
2025-05-14 16:03:05 +0900

    [level 2] Title: H-Index, Time: 0.14 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,9 @@
 def solution(citations):
     citations.sort(reverse=True)
+    # hindex가 존재하고, hindex를 넘는 논문
     for i in range(len(citations)):
         if citations[i] < i+1:
             return i
+    # 인용 횟수가 모두 같으면 전체를 반환 
+    # ex) [5, 5, 5, 5, 5] => hindex = 5
     return len(citations)

8912efa95ff754d9851167f9040fc172f57d3f74
심여은 <pudding4271@gmail.com>
2025-05-22 14:34:44 +0900

    [Gold IV] Title: Puyo Puyo, Time: 56 ms, Memory: 35016 KB -BaekjoonHub

--- Python/백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ Python/백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -1,63 +1,53 @@
-# 연쇄가 계속 이루어짐 -> bfs? 맞네
-# 모르겠는 부분: 터지고 나서 다른 요소들이 내려올 때, 그걸 어떻게 감지? 
 from collections import deque
 
 def bfs(color, x, y):
-    global boom_flag
-    boom_list = [(x, y)]
+    global is_exploded
     queue = deque([(x, y)])
+    bombs = [(x, y)]
     visited[x][y] = True
-    n = 1
-
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             dx, dy = x+d[i][0], y+d[i][1]
             if 0 <= dx < 12 and 0 <= dy < 6 and visited[dx][dy] == False:
                 if field[dx][dy] == color:
-                    visited[dx][dy] = True
                     queue.append((dx, dy))
-                    boom_list.append((dx, dy))
-                    n += 1
-    # 4개가 되면 해당 부분 터트려주기 -> .로 만듬
-    if n >= 4:
-        for boom_x, boom_y in boom_list:
-            field[boom_x][boom_y] = '.'
-        boom_flag = True
-
+                    visited[dx][dy] = True
+                    bombs.append((dx, dy))
+    # 한 점에 대해서 다 탐색하고 나서
+    if len(bombs) >= 4:
+        is_exploded = True
+        for bomb_x, bomb_y in bombs:
+            field[bomb_x][bomb_y] = '.'
 
 field = [[] for _ in range(12)]
-d = [(-1, 0), (1,0), (0, -1), (0, 1)]
-
 for i in range(12):
     field[i] = list(input())
-
-boom_count = 0
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+answer = 0
 
 while True:
-    boom_flag = False
+    is_exploded = False
     visited = [[False]*6 for _ in range(12)]
     for i in range(12):
         for j in range(6):
             if field[i][j] != '.':
                 bfs(field[i][j], i, j)
-    # 각 열마다, 밑에서부터 스캔하며 뿌요를 rotate_queue에 넣는다.
-    for i in range(6):
-        rotate_queue = deque()
-        for j in range(11, -1, -1):
-            if field[j][i] != '.':
-                rotate_queue.append(field[j][i])
-        # 다시 밑에서부터 차례대로 뿌요를 쌓는다.
-        for j in range(11, -1, -1):
-            if rotate_queue:
-                field[j][i] = rotate_queue.popleft()
-            # 다 쌓고 나서 빈칸으로 채운다.
+    
+    for j in range(6):
+        puyo = deque([])
+        # 세로로 세기
+        for i in range(11, -1, -1):
+            if field[i][j] != '.':
+                puyo.append(field[i][j])
+        for i in range(11, -1, -1):
+            if puyo:
+                field[i][j] = puyo.popleft()
             else:
-                field[j][i] = '.'
-    # 더 이상 터질게 없다면
-    if not boom_flag:
-        break
+                field[i][j] = '.'
+    if is_exploded:
+        answer += 1
     else:
-        boom_count += 1 # 전체적으로 터진것도 모두 1번으로 침
+        break
 
-print(boom_count)
+print(answer)

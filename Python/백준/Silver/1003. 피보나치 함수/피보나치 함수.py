5b796d543a66de789e537baa0297756a40893293
심여은 <pudding4271@gmail.com>
2025-05-23 18:35:07 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+tc = int(input())
+
+for _ in range(tc):
+    n = int(input())
+
+    zero = [0] * 41
+    one = [0] * 41
+
+    zero[0], zero[1] = 1, 0
+    one[0], one[1] = 0, 1
+
+    for i in range(2, n+1):
+        zero[i] = zero[i-1] + zero[i-2]
+        one[i] = one[i-1] + one[i-2]
+    
+    print(zero[n], one[n])

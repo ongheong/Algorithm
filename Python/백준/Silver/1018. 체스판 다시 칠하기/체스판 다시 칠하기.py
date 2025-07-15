b83caf732fcf3540f7ee5438b406b7e4c09824fd
심여은 <pudding4271@gmail.com>
2025-07-15 23:28:20 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,35 +1,26 @@
-import sys
-import copy
-input = sys.stdin.readline
+N, M = map(int, input().split())
+original = []
+count = []
 
-def change_color(row, col):
-    # draw1은 왼쪽 위가 B일 때 바꿔야 하는 칸 수
-    # draw2는 W일 때 바꿔야 하는 칸 수
-    draw1, draw2 = 0, 0
-    for i in range(row, row+8):
-        for j in range(col, col+8):
-            if (i + j) % 2 == 0: # 짝수인 칸은 첫 칸과 같은 색
-                if board[i][j] != 'B':
-                    draw1 += 1
+for _ in range(N):
+    original.append(input())
+
+for a in range(N-7):
+    for b in range(M-7):
+        index1 = 0
+        index2 = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if original[i][j] != 'W':
+                        index1 += 1
+                    if original[i][j] != 'B':
+                        index2 += 1
                 else:
-                    draw2 += 1
-            else: # 홀수인 칸은 첫 칸과 다른 색
-                if board[i][j] != 'W':
-                    draw1 += 1
-                else:
-                    draw2 += 1
-    return min(draw1, draw2)
+                    if original[i][j] != 'B':
+                        index1 += 1
+                    if original[i][j] != 'W':
+                        index2 += 1
+        count.append(min(index1, index2))
 
-
-n, m = map(int, input().split())
-board = [[] for _ in range(n)]
-answer = []
-
-for i in range(n):
-    board[i] = list(input().rstrip())
-
-for i in range(0, n-7):
-    for j in range(0, m-7):
-        answer.append(change_color(i, j))
-
-print(min(answer))
+print(min(count))
